2d4f0674d6e2a8f93af20ba9efebc8bf0029a3b8
Mohammad Mesum Hussain <64989499+Mesumm8@users.noreply.github.com>
2026-09-07 21:14:16 +0400

    setting project
    installed nodejs, setup project directory, installed necessary dependencies and packages, and lastly listed the task list to completed

---
+++ project.js
@@ -0,0 +1,10 @@
+/* Slot Machine Game */
+
+// 1. deposit some money
+// 2. Determine number of lines to bet on
+// 3. collect a bet amount
+// 4. spin the slot machine
+// 5. check if the user won
+// 6. give the user their winnings
+// 7. play again
+
