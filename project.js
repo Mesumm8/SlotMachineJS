bd7d9721e6e3d154c3fcad0def690f60eb250cb3
Mohammad Mesum Hussain <64989499+Mesumm8@users.noreply.github.com>
2026-09-08 06:08:27 +0400

    starting with getting user input

--- project.js
+++ project.js
@@ -1,6 +1,6 @@
 /* Slot Machine Game */
 
-// 1. deposit some money
+// 1. deposit some money 
 // 2. Determine number of lines to bet on
 // 3. collect a bet amount
 // 4. spin the slot machine
@@ -8,3 +8,33 @@
 // 6. give the user their winnings
 // 7. play again
 
+const prompt = require("prompt-sync")();
+
+function deposit() {
+    while(true){
+        const depositAmount = prompt("Enter a deposit amount: ");
+        const numberDepositAmount = parseFloat(depositAmount);
+
+        if(isNaN(numberDepositAmount) || numberDepositAmount <= 0) {
+        console.log("Invalid deposit amount, try again!");
+        } else {
+            return numberDepositAmount;
+        }
+    }
+}
+
+function getNumberOfLines() {
+    while (true) {
+        const lines = prompt("Enter the number of lines to bet on (1-3): ");
+        const numberOfLines = parseFloat(lines);
+
+        if (isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3) {
+            console.log("Invalid number of lines, try again!");
+        } else {
+            return numberOfLines;
+        }
+    }
+}
+
+let balance = deposit();
+const numberOfLines = getNumberOfLines();
