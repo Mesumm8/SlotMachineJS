2ebfb3ecb66b48b8b1cc3cc3705e15f1138914c4
Mohammad Mesum Hussain <64989499+Mesumm8@users.noreply.github.com>
2026-09-08 06:47:14 +0400

    added variables and objects to spin the wheel

--- project.js
+++ project.js
@@ -10,6 +10,24 @@
 
 const prompt = require("prompt-sync")();
 
+//variables
+const ROWS = 3;
+const COLS = 3;
+// Symbols
+const SYMBOLS_COUNT = {
+    "A": 2,
+    "B": 4,
+    "C": 6,
+    "D": 8
+} 
+const SYMBOL_VALUES = {
+    "A": 5,
+    "B": 4,
+    "C": 3,
+    "D": 2
+}
+
+
 function deposit() {
     while(true){
         const depositAmount = prompt("Enter a deposit amount: ");
@@ -36,5 +54,36 @@ function getNumberOfLines() {
     }
 }
 
+function getBet(balance, lines) {
+    while (true) {
+        const bet = prompt("Enter the bet per line: ");
+        const numberBet = parseFloat(bet);
+
+        if (isNaN(numberBet) || numberBet <= 0 || numberBet > balance / lines) {
+            console.log("Invalid bet, try again!");
+        } else {
+            return numberBet;
+        }
+    }
+}
+
+function spin() {
+    const symbols = [];
+    for (const [symbol, count] of Object.entries(SYMBOLS_COUNT)) {
+        for (let i = 0; i < count; i++) {
+            symbols.push(symbol);
+        }
+    }
+    const reels = [[],[],[]];
+    for (let i = 0; i < COLS; i++) {
+        const reelSymbols = [...symbols];
+        for (let j = 0; j < ROWS; j++) {
+            
+        }
+    }
+}
+
+
 let balance = deposit();
 const numberOfLines = getNumberOfLines();
+const bet = getBet(balance, numberOfLines);
